ab5aef412dca260e6769540a798e87565765f07a
fredrik.r.lundin@gmail.com <fredrik.r.lundin@gmail.com>
2025-04-13 14:10:48 +0200

    Full program

--- main.c
+++ main.c
@@ -3,6 +3,16 @@
 
 // This is where you add the two functions
 
+float fahrenheit_to_celsius(float temp_f)
+{
+    return (temp_f - 32.f) * 5.f / 9.f;
+}
+
+float celsius_to_fahrenheit(float temp_c)
+{
+    return temp_c * 9.f / 5.f + 32.f;
+}
+
 int main() {
     float temp_f = 100.0;
     float temp_c = 37.0;
